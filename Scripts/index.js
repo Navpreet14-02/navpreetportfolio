07272982f088b5615d360dc066a6f19ccc0f34e6
Navpreet14-02 <79501562+Navpreet14-02@users.noreply.github.com>
2022-07-26 12:31:20 +0530

    First Commit

---
+++ Scripts/index.js
@@ -0,0 +1,7 @@
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.nav-items');
+
+
+toggleButton.addEventListener('click',() =>{
+    navbarLinks.classList.toggle('active')
+})
